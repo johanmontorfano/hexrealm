43aac5a306f5a86e9420f256058784515363a79d
Johan Montorfano <me@johanmontorfano.com>
2024-12-14 23:57:33 +0100

    add: deeper copies

--- src/objects/loader.ts
+++ src/objects/loader.ts
@@ -3,16 +3,22 @@ import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
 const loader = new GLTFLoader();
 
+function traverse(gltf: any) {
+    gltf.traverse((c: any) => {
+        c.receiveShadow = true;
+        c.castShadow = true;
+        if (c.isGroup) c.children.forEach((d: any) => traverse(d));
+        if (c.isMesh) {
+            c.geometry.computeVertexNormals();
+            c.geometry.computeBoundingBox();
+        }
+    });
+}
+
 /** Returns the first scene of a GLTF object and enable shadows. */
 export async function getGLTF(path: string, shadows = true): Promise<Scene> {
     const gltf = await loader.loadAsync(path);
 
-    if(shadows) gltf.scene.traverse((c: any) => {
-        if (!c.isMesh) return;
-        c.receiveShadow = true;
-        c.castShadow = true;
-        c.geometry.computeVertexNormals();
-        c.geometry.computeBoundingBox();
-    });
+    if (shadows) traverse(gltf.scene);
     return gltf.scene as any;
 }
