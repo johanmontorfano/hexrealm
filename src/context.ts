a7037822c80de0b34b59cc36bddb8c9b15bdab8e
Johan Montorfano <me@johanmontorfano.com>
2024-12-14 23:40:44 +0100

    edit: adapted context menu for lil-gui

--- src/context.ts
+++ src/context.ts
@@ -1,72 +1,67 @@
+import { Color } from "three";
+
+export interface Biomes {
+    name: string
+    threshold: number;
+    color: Color;
+    altitudeDecay?: Color; // not implemented
+    enabledTreeType?: "palm_tree" | "fir_tree";
+    altitudeModifier?: number;
+}
+
 export interface Context {
     seed: number;
-    noise: number; // Recomended between 0 and 1
+    noise: number; // Recomended between 0 and 10
     scale: number;
-    water_level: number; // Recommended between 0 and 1
-    sand_level: number;  // Recommended between 0 and 1
-    dirt_level: number;  // Recommended between 0 and 1
-    _seed: number;
-    _noise: number;
-    _scale: number;
-    _water_level: number;
-    _sand_level: number;
-    _dirt_level: number;
-    _render_effects: boolean;
+    biomes_data: Biomes[],
+    trees_offset: number,
+    rotation: boolean;
+    render_effects: boolean;
     _contextDepsUpdateRequested: boolean;
 }
 
 (window as any).ctx = {
-    _seed: 72,
-    _noise: 2.6,
-    _scale: 4,
-    _water_level: .2,
-    _sand_level: .3,
-    _dirt_level: .6,
-    _render_effects: false,
+    seed: 72,
+    noise: 2.6,
+    scale: 4,
+    biomes_data: [
+        {
+            name: "water",
+            threshold: .1,
+            color: new Color(0x000033),
+            altitudeModifier: .18
+        },
+        {
+            name: "shore",
+            threshold: .18,
+            color: new Color(0xFFD68F),
+            altitudeModifier: .18
+        },
+        {
+            name: "beach",
+            threshold: .24,
+            color: new Color(0xAAAA97),
+            enabledTreeType: "palm_tree"
+        },
+        {
+            name: "grass",
+            threshold: .38,
+            color: new Color(0x4B8B3B),
+            enabledTreeType: "fir_tree"
+        },
+        {
+            name: "mountain",
+            threshold: 1,
+            color: new Color(0xFFFFFF),
+            enabledTreeType: "fir_tree"
+        }
+    ],
+    trees_offset: .3,
+    rotation: true,
+    render_effects: false,
     _contextDepsUpdateRequested: false,
-    get seed(): number {
-        return this._seed;
-    },
-    set seed(seed: number) {
-        this._seed = seed;
-        this._contextDepsUpdateRequested = true;
-    },
-    get noise(): number {
-        return this._noise;
-    },
-    set noise(noise: number) {
-        this._noise = noise;
-        this._contextDepsUpdateRequested = true;
-    },
-    get scale(): number {
-        return this._scale;
-    },
-    set scale(scale: number) {
-        this._scale = scale;
-        this._contextDepsUpdateRequested = true;
-    },
-    get water_level(): number {
-        return this._water_level;
-    },
-    set water_level(level: number) {
-        this._water_level = level;
-        this._contextDepsUpdateRequested = true;
-    },
-    get sand_level(): number {
-        return this._sand_level;
-    },
-    set sand_level(level: number) {
-        this._sand_level = level;
-        this._contextDepsUpdateRequested = true;
-    },
-    get dirt_level(): number {
-        return this._dirt_level;
-    },
-    set dirt_level(level: number) {
-        this._dirt_level = level;
-        this._contextDepsUpdateRequested = true;
-    }
 } as Context;
+
 
 /** WARN: This function aknowledges a deps update. */
 export function contextDepsUpdateRequested() {
